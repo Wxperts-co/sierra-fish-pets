789ea2d91189df694b96f813287b44447ae66313
Saurabhsaasho <saurabh.kumar@saasho.com>
2026-07-08 23:44:53 +0530

    bug fix

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false,
+  secure: Number(process.env.SMTP_PORT) === 465, // Set to true if port is 465 (SSL)
 
   auth: {
     user: process.env.SMTP_USER,
